fa351a29a808e2350bdbba8776cfa949f8bd3849
Miruna <56968108+miruna53@users.noreply.github.com>
2022-04-05 16:29:44 +0300

    Add files via upload

--- module5/js/script.js
+++ module5/js/script.js
@@ -102,7 +102,6 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
